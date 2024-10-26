ffd9d027c147f938e874ec81dfeb168a56eefbd0
rifaianwar <rifaianwar@outlook.com>
2024-10-27 05:27:57 +0700

    Setup Project

---
+++ src/test/java/com/r3s/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r3s.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
